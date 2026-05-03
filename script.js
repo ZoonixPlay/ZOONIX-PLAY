5bc3326430fa6f5f50608e2e2659a4695fa27aa1
ZOONIX PLAY <115875983+ZoonixPlay@users.noreply.github.com>
2026-05-03 20:43:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const JOGOS = [
         logoCampeonato: "https://upload.wikimedia.org/wikipedia/pt/1/18/Campeonato_Brasileiro_de_Futebol_de_2022_-_S%C3%A9rie_A.png", 
         data: "2026-05-03", 
         horario: "18:30",    
-        link: "https://nossoplayeronlinehd.cfd/tv/premiere" 
+        link: "#" 
     },
 ];
 
